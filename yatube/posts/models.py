558ced138308a8b225476295e94f772aae841c10
roman <roman.petrakov+git@gmail.com>
2022-06-01 21:05:10 +0300

    Added templates, static and model

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts'
+    )
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return f'Группа {self.title}'
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,25 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Post
 
 
 def index(request):
     """View of main page"""
-    return HttpResponse('Main page')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    template = 'posts/index.html'
+    context = {
+        'title': 'Главная страница',
+        'text': 'Это главная страница проекта Yatube',
+        'posts': posts,
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug=None):
     """Posts filtered by groups (slug)"""
-    return HttpResponse(f'Group posts {slug}')
+    template = 'posts/group_list.html'
+    context = {
+        'title': 'Здесь будет информация о группах проекта Yatube',
+        'text': 'Здесь будет информация о группах проекта Yatube',
+    }
+    return render(request, template, context)

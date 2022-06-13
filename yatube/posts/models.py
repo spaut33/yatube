7a449eb10bd85bd108574c1148783c2028c24a29
roman <roman.petrakov+git@gmail.com>
2022-06-13 23:43:24 +0300

    final fixes

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -21,8 +21,11 @@ class Post(models.Model):
     )
     group = models.ForeignKey(
         Group,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name='group_posts',
+        related_name='posts',
     )
+
+    class Meta:
+        ordering = ('-pub_date',)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,22 +5,14 @@ from .models import Group, Post
 
 def index(request):
     """Главная страница"""
-    # Получаем 10 постов с сортировкой по дате
-    # TODO: ordering наверное лучше вынести в Meta и убрать order_by тут и ниже
-    posts = Post.objects.order_by('-pub_date')[:10]
-    context = {
-        'title': 'Последние обновления на сайте',
-        'posts': posts,
-    }
+    posts = Post.objects.all()[:10]
+    context = {'posts': posts}
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug=None):
     """Posts filtered by groups (slug)"""
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
-    context = {
-        'group': group,
-        'posts': posts,
-    }
+    posts = group.posts.all()[:10]
+    context = {'group': group, 'posts': posts}
     return render(request, 'posts/group_list.html', context)
